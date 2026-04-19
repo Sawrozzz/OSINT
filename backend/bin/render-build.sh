a36c03b3c429e899d6deb6d93a7a6b11d92f5353
Sawrozzz <sawroz.adhikari@.com>
2026-04-19 15:57:19 +0545

    fixes on prod

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 bundle install
-# If you are strictly an API, you might not need assets:precompile, 
-# but it's safe to keep for standard Rails setups.
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-
-# This line runs your migrations on every deploy
 bundle exec rails db:migrate
